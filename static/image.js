ff0f1beecb28a50ee899107fb525de1a743899da
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-11-23 16:24:25 +0100

    Wrap most of image.js in a function
    This lets us share some global elements between all invocations of
    addEditButton(), and will also make more sense with some upcoming
    changes where I’ll add a form to each image.

--- static/image.js
+++ static/image.js
@@ -1,136 +1,141 @@
-function addEditButtons() {
-    document.querySelectorAll('.wd-image-positions--depicted-without-region').forEach(addEditButton);
-}
-
-function addEditButton(element) {
-    const entity = element.closest('.wd-image-positions--entity'),
-          subjectId = entity.dataset.entityId,
-          subjectDomain = entity.dataset.entityDomain,
-          depictedId = element.firstChild.dataset.entityId,
-          image = entity.querySelector('.wd-image-positions--image'),
+function setup() {
+    const baseUrl = document.querySelector('link[rel=index]').href.replace(/\/$/, ''),
+          loginElement = document.getElementById('login'),
           csrfTokenElement = document.getElementById('csrf_token');
 
-    if (depictedId === undefined && csrfTokenElement === null) {
-        // editing somevalue/novalue not supported in QuickStatements mode
-        return;
+    function addEditButtons() {
+        document.querySelectorAll('.wd-image-positions--depicted-without-region').forEach(addEditButton);
     }
 
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.classList.add('btn', 'btn-secondary', 'btn-sm');
-    button.textContent = 'add region';
-    button.addEventListener('click', onClick)
-    element.append(document.createTextNode(' '));
-    element.append(button);
+    function addEditButton(element) {
+        const entity = element.closest('.wd-image-positions--entity'),
+              subjectId = entity.dataset.entityId,
+              subjectDomain = entity.dataset.entityDomain,
+              depictedId = element.firstChild.dataset.entityId,
+              image = entity.querySelector('.wd-image-positions--image');
 
-    let cropper = null;
+        if (depictedId === undefined && csrfTokenElement === null) {
+            // editing somevalue/novalue not supported in QuickStatements mode
+            return;
+        }
 
-    function onClick() {
-        if (cropper === null) {
-            button.textContent = 'loading...';
-            image.classList.add('wd-image-positions--active');
-            button.classList.add('wd-image-positions--active');
-            cropper = new Cropper(image.firstElementChild, {
-                viewMode: 2,
-                movable: false,
-                rotatable: false,
-                scalable: false,
-                zoomable: false,
-                checkCrossOrigin: false,
-                ready: function() {
-                    button.textContent = 'use this region';
-                },
-            });
-            document.addEventListener('keydown', onKeyDown);
-        } else {
-            if (button.textContent === 'loading...') {
-                return;
-            }
-            image.classList.remove('wd-image-positions--active');
-            const cropData = cropper.getData(),
-                  imageData = cropper.getImageData(),
-                  x = 100 * cropData.x / imageData.naturalWidth,
-                  y = 100 * cropData.y / imageData.naturalHeight,
-                  w = 100 * cropData.width / imageData.naturalWidth,
-                  h = 100 * cropData.height / imageData.naturalHeight,
-                  depicted = document.createElement('div');
-            depicted.classList.add('wd-image-positions--depicted')
-            depicted.append(element.firstChild.cloneNode(true));
-            // note: the browser rounds the percentages a bit,
-            // and we’ll use the rounded values for the IIIF region
-            depicted.style.left = `${x}%`;
-            depicted.style.top = `${y}%`;
-            depicted.style.width = `${w}%`;
-            depicted.style.height = `${h}%`;
-            cropper.destroy();
-            cropper = null;
-            image.append(depicted);
-            function pct(name) {
-                return depicted.style[name].replace('%', '');
-            }
-            const iiifRegion = `pct:${pct('left')},${pct('top')},${pct('width')},${pct('height')}`,
-                  quickStatements = `${subjectId}\tP180\t${depictedId}\tP2677\t"${iiifRegion}"`;
+        const button = document.createElement('button');
+        button.type = 'button';
+        button.classList.add('btn', 'btn-secondary', 'btn-sm');
+        button.textContent = 'add region';
+        button.addEventListener('click', onClick)
+        element.append(document.createTextNode(' '));
+        element.append(button);
 
-            if (csrfTokenElement !== null) {
-                button.textContent = 'adding qualifier…';
-                const baseUrl = document.querySelector('link[rel=index]').href.replace(/\/$/, ''),
-                      statementId = element.dataset.statementId,
-                      csrfToken = csrfTokenElement.textContent,
-                      formData = new FormData();
-                formData.append('statement_id', statementId);
-                formData.append('iiif_region', iiifRegion);
-                formData.append('_csrf_token', csrfToken);
-                fetch(`${baseUrl}/api/v2/add_qualifier/${subjectDomain}`, {
-                    method: 'POST',
-                    body: formData,
-                    credentials: 'include',
-                } ).then(response => {
-                    if (response.ok) {
+        let cropper = null;
+
+        function onClick() {
+            if (cropper === null) {
+                button.textContent = 'loading...';
+                image.classList.add('wd-image-positions--active');
+                button.classList.add('wd-image-positions--active');
+                cropper = new Cropper(image.firstElementChild, {
+                    viewMode: 2,
+                    movable: false,
+                    rotatable: false,
+                    scalable: false,
+                    zoomable: false,
+                    checkCrossOrigin: false,
+                    ready: function() {
+                        button.textContent = 'use this region';
+                    },
+                });
+                document.addEventListener('keydown', onKeyDown);
+            } else {
+                if (button.textContent === 'loading...') {
+                    return;
+                }
+                image.classList.remove('wd-image-positions--active');
+                const cropData = cropper.getData(),
+                      imageData = cropper.getImageData(),
+                      x = 100 * cropData.x / imageData.naturalWidth,
+                      y = 100 * cropData.y / imageData.naturalHeight,
+                      w = 100 * cropData.width / imageData.naturalWidth,
+                      h = 100 * cropData.height / imageData.naturalHeight,
+                      depicted = document.createElement('div');
+                depicted.classList.add('wd-image-positions--depicted')
+                depicted.append(element.firstChild.cloneNode(true));
+                // note: the browser rounds the percentages a bit,
+                // and we’ll use the rounded values for the IIIF region
+                depicted.style.left = `${x}%`;
+                depicted.style.top = `${y}%`;
+                depicted.style.width = `${w}%`;
+                depicted.style.height = `${h}%`;
+                cropper.destroy();
+                cropper = null;
+                image.append(depicted);
+                function pct(name) {
+                    return depicted.style[name].replace('%', '');
+                }
+                const iiifRegion = `pct:${pct('left')},${pct('top')},${pct('width')},${pct('height')}`,
+                      quickStatements = `${subjectId}\tP180\t${depictedId}\tP2677\t"${iiifRegion}"`;
+
+                if (csrfTokenElement !== null) {
+                    button.textContent = 'adding qualifier…';
+                    const statementId = element.dataset.statementId,
+                          csrfToken = csrfTokenElement.textContent,
+                          formData = new FormData();
+                    formData.append('statement_id', statementId);
+                    formData.append('iiif_region', iiifRegion);
+                    formData.append('_csrf_token', csrfToken);
+                    fetch(`${baseUrl}/api/v2/add_qualifier/${subjectDomain}`, {
+                        method: 'POST',
+                        body: formData,
+                        credentials: 'include',
+                    } ).then(response => {
+                        if (response.ok) {
+                            element.remove();
+                        } else {
+                            response.text().then(text => {
+                                message = `An error occurred:\n\n${text}`;
+                                if (depictedId !== undefined) {
+                                    // we’re not in an event handler, we can’t write to the clipboard directly
+                                    message += `\n\nHere is the new region in QuickStatements syntax:\n\n${quickStatements}`;
+                                }
+                                window.alert(message);
+                                element.remove();
+                            });
+                        }
+                    });
+                } else {
+                    let message = '';
+                    if (loginElement !== null) {
+                        message = 'You are not logged in. ';
+                    }
+                    message += 'Copy the new region to the clipboard (in QuickStatements syntax)?';
+                    if (window.confirm(message)) {
+                        navigator.clipboard.writeText(quickStatements);
                         element.remove();
                     } else {
-                        response.text().then(text => {
-                            message = `An error occurred:\n\n${text}`;
-                            if (depictedId !== undefined) {
-                                // we’re not in an event handler, we can’t write to the clipboard directly
-                                message += `\n\nHere is the new region in QuickStatements syntax:\n\n${quickStatements}`;
-                            }
-                            window.alert(message);
-                            element.remove();
-                        });
+                        depicted.remove();
+                        button.textContent = 'add region';
+                        button.classList.remove('wd-image-positions--active');
                     }
-                });
-            } else {
-                const loginElement = document.getElementById('login');
-                let message = '';
-                if (loginElement !== null) {
-                    message = 'You are not logged in. ';
                 }
-                message += 'Copy the new region to the clipboard (in QuickStatements syntax)?';
-                if (window.confirm(message)) {
-                    navigator.clipboard.writeText(quickStatements);
-                    element.remove();
-                } else {
-                    depicted.remove();
+            }
+            function onKeyDown(eKey) {
+                if (eKey.key === 'Escape') {
+                    cropper.destroy();
+                    cropper = null;
+                    image.classList.remove('wd-image-positions--active');
+                    document.removeEventListener('keydown', onKeyDown);
                     button.textContent = 'add region';
                     button.classList.remove('wd-image-positions--active');
                 }
             }
         }
-        function onKeyDown(eKey) {
-            if (eKey.key === 'Escape') {
-                cropper.destroy();
-                cropper = null;
-                image.classList.remove('wd-image-positions--active');
-                document.removeEventListener('keydown', onKeyDown);
-                button.textContent = 'add region';
-                button.classList.remove('wd-image-positions--active');
-            }
-        }
     }
+
+    addEditButtons();
 }
 
 if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', addEditButtons);
+    document.addEventListener('DOMContentLoaded', setup);
 } else {
-    addEditButtons();
+    setup();
 }
